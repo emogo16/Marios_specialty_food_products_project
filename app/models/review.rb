c442d742c41ae9539a106b35a03eeb9a11c55970
Erika Mogollon <fany200025@hotmail.com>
2022-06-11 14:34:58 -0700

    Added foreign key

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,2 +1,14 @@
 class Review < ApplicationRecord
+  belongs_to :product
+
+  validates :author, :content_body, :rating,presence: true
+  validates_length_of :author, maximum: 100
+  validates_length_of :content_body, minimum:50, maximum:250
+
+  before_save(:titlelize_review)
+
+  private 
+    def titlelize_review
+      self.author =  self.author.titleize
+    end
 end
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -2,9 +2,4 @@ require 'rails_helper'
 
 describe Review do
   it { should belong_to(:product) }
-  it { should validate_presence_of :author }
-  it { should validate_presence_of :content }
-  it { should validate_presence_of :rating }
-
-  it{should belong_to(:product)}
 end
